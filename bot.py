53925a032ea4a5723dd05e32481648c145e62839
romacaregorodcev549-spec <romacaregorodcev549@gmail.com>
2026-05-19 22:53:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -791,7 +791,7 @@ def process_message(msg):
     if user_state.get(chat_id) == 'waiting_dictation':
         user_state[chat_id] = None
         correct = user_last_text.get(chat_id, '')
-     if text.lower().strip() == correct.lower().strip():
+    if text.lower().strip() == correct.lower().strip():
             add_points(chat_id, 10)
             send_message(chat_id, f'✅ Правильно! +10 очков!')
         else:
